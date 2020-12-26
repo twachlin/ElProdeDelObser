9d3454310c91562283899457d7200b5662661e84
Tomas <tomaswachlin@gmail.com>
2020-12-26 09:52:07 -0300

    Se agrego a devise confirmable para enviar mails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,18 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  ActionMailer::Base.smtp_settings = {
+   :tls => true,
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :domain => "gmail.com",
+   :authentication => :login,
+   :user_name => "[username]",
+   :password => "[password]"
+ }
   
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,15 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+   :tls => true,
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :domain => "gmail.com",
+   :authentication => :login,
+   :user_name => "[username]",
+   :password => "[password]"
+ } 
+
 end

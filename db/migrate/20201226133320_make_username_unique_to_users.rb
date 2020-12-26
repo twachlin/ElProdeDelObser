81dd53f38380ddb416376539729177598383171a
Tomas <tomaswachlin@gmail.com>
2020-12-26 12:45:01 -0300

    Se agrego en username a devise y el modelo team

---
+++ db/migrate/20201226133320_make_username_unique_to_users.rb
@@ -0,0 +1,5 @@
+class MakeUsernameUniqueToUsers < ActiveRecord::Migration[6.0]
+  def change
+    change_column :users, :username, :string, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_25_224145) do
+ActiveRecord::Schema.define(version: 2020_12_26_153811) do
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_12_25_224145) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "username", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
